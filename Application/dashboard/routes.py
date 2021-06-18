55ada5e3b0ae466ad4331454da648042b29b92a0
anrichp <anrichp@gmail.com>
2021-06-18 21:47:31 +0100

    create dashboard index page

---
+++ Application/dashboard/routes.py
@@ -0,0 +1,12 @@
+from flask import Blueprint, render_template, abort
+from jinja2 import TemplateNotFound
+
+dashboard = Blueprint('dashboard', __name__, template_folder='templates')
+
+@dashboard.route('/')
+def index():
+    try:
+        return render_template('index.html')
+    except TemplateNotFound:
+        abort(404)
+
