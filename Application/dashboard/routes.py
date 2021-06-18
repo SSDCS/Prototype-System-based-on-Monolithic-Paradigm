b141c8c81ca16daade55af4f40bbd5a111850a62
anrichp <anrichp@gmail.com>
2021-06-18 23:33:21 +0100

    remove trailing white space

--- Application/dashboard/routes.py
+++ Application/dashboard/routes.py
@@ -3,10 +3,10 @@ from jinja2 import TemplateNotFound
 
 dashboard = Blueprint('dashboard', __name__, template_folder='templates')
 
+
 @dashboard.route('/')
 def index():
     try:
         return render_template('index.html')
     except TemplateNotFound:
         abort(404)
-
