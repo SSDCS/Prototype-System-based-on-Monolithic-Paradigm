a0ec9e9e5aa51588fcdd146eb1f9ba3f87596932
anrichp <anrichp@gmail.com>
2021-06-23 21:44:28 +0100

    define templates

--- Application/auth/__init__.py
+++ Application/auth/__init__.py
@@ -1,14 +1,7 @@
 from flask import Blueprint
 
-bp = Blueprint('auth', __name__)
+bp = Blueprint('auth', __name__, static_folder="static", template_folder="templates")
 
-from Application.auth import routes 
-
-# # from my_app.forms import Login, Registration #importing forms
-# # from my_app.models import Astronaut, Admin #importing models
-# blueprint=Blueprint("blueprint", __name__, static_folder="static", template_folder="templates") # creating a blueprint
-
-
-# # from my_app.models import Admin
+from Application.auth import routes
 
 
