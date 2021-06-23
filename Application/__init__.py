04e3629be5fa7ae3c3f7ec1098dc00473ce6750f
anrichp <anrichp@gmail.com>
2021-06-23 20:55:21 +0100

    import bcrypt and sqlalchemy

--- Application/__init__.py
+++ Application/__init__.py
@@ -1,5 +1,11 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 from config import Config
+
+db = SQLAlchemy()  # creates a database
+bcrypt = Bcrypt()  # instatiate the bcrypt to help in hashing of passwords
+
 
 def create_app(config_class=Config):
     app = Flask(__name__)
