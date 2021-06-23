a9cc5db7d35f9f93e9d029b4276d4253df067e9a
anrichp <anrichp@gmail.com>
2021-06-23 21:21:06 +0100

    refactor init

--- Application/auth/__init__.py
+++ Application/auth/__init__.py
@@ -1,14 +1,14 @@
-from flask import Flask,render_template, url_for, session, request, redirect, flash, Blueprint
-from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
-from functools import wraps
- 
+from flask import Blueprint
 
-from my_app.forms import Login, Registration #importing forms
-from my_app.models import Astronaut, Admin #importing models
-blueprint=Blueprint("blueprint", __name__, static_folder="static", template_folder="templates") # creating a blueprint
+bp = Blueprint('auth', __name__)
+
+from Application.auth import routes 
+
+# # from my_app.forms import Login, Registration #importing forms
+# # from my_app.models import Astronaut, Admin #importing models
+# blueprint=Blueprint("blueprint", __name__, static_folder="static", template_folder="templates") # creating a blueprint
 
 
-from my_app.models import Admin
+# # from my_app.models import Admin
 
 
