8ad4602b5ff67252321342f93bf1e9113796838d
anrichp <anrichp@gmail.com>
2021-06-23 21:03:45 +0100

    create configuration file

--- config.py
+++ config.py
@@ -5,4 +5,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
-    pass
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'thisissupposedtobemysecret' #configure a secret key
+    SQLALCHEMY_DATABASE_URI = os.environ.get('sqlite:///users.db')
