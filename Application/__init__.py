56a2bb723a2bfd8b60d7e562504eacc328290c4d
anrichp <anrichp@gmail.com>
2021-06-23 21:51:27 +0100

    initialise db and bcrypt

--- Application/__init__.py
+++ Application/__init__.py
@@ -11,6 +11,9 @@ def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
+    db.init_app(app)
+    bcrypt.init_app(app)
+
     from Application.auth import bp as auth_bp
     app.register_blueprint(auth_bp, url_prefix='/auth')
 
