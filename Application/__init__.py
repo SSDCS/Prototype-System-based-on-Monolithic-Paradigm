ad1f4e3cc3fa1d382974f602236d8ade05576321
anrichp <anrichp@gmail.com>
2021-06-23 19:42:16 +0100

    import dashboard blueprint

--- Application/__init__.py
+++ Application/__init__.py
@@ -8,4 +8,7 @@ def create_app(config_class=Config):
     from Application.auth import bp as auth_bp
     app.register_blueprint(auth_bp, url_prefix='/auth')
 
+    from Application.dashboard import bp as dashboard_bp
+    app.register_blueprint(dashboard_bp)
+
     return app
