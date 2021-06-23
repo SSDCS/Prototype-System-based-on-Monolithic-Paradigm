ffab226674f5a0f443d2f99fe166ad134d774bec
anrichp <anrichp@gmail.com>
2021-06-23 22:39:28 +0100

    track changes overhead false

--- config.py
+++ config.py
@@ -7,3 +7,4 @@ load_dotenv(os.path.join(basedir, '.env'))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'thisissupposedtobemysecret' #configure a secret key
     SQLALCHEMY_DATABASE_URI = os.environ.get('sqlite:///users.db') #configure users database
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
