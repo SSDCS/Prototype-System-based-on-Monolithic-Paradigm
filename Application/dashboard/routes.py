8182110639a96cdedffaaf039e68d378c3f3ac47
anrichp <anrichp@gmail.com>
2021-06-27 17:37:24 +0100

    import login decorator

--- Application/dashboard/routes.py
+++ Application/dashboard/routes.py
@@ -3,9 +3,11 @@ Dashboard
 """
 from flask import render_template
 from Application.dashboard import bp
+from ..decorators import login_required
 
 
 @bp.route('/', methods=['GET', 'POST'])
 @bp.route('/index', methods=['GET', 'POST'])
+@login_required
 def index():
     return render_template('dashboard.html')
