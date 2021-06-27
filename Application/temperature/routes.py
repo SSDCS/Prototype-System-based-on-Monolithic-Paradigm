43eecdc15932c8afbce03e6fdbb377c15ea0a1b0
lucosmo <lukasz.kosmaczewski@gmail.com>
2021-06-27 15:11:25 +0100

    routes

---
+++ Application/temperature/routes.py
@@ -0,0 +1,11 @@
+"""
+Temperature
+"""
+from flask import render_template
+from Application.temperature import bp
+
+
+@bp.route('/', methods=['GET', 'POST'])
+@bp.route('/index', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
