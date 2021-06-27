3aa53fcab832a6eaef228fad8f11bf177877297d
anrichp <anrichp@gmail.com>
2021-06-27 17:28:45 +0100

    updated the dashboard template

--- Application/dashboard/routes.py
+++ Application/dashboard/routes.py
@@ -8,4 +8,4 @@ from Application.dashboard import bp
 @bp.route('/', methods=['GET', 'POST'])
 @bp.route('/index', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    return render_template('dashboard.html')
