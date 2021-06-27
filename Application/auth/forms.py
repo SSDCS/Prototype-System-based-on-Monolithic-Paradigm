080ccfcd1f3e6686546cd876fd43fb6476f68cfd
anrichp <anrichp@gmail.com>
2021-06-27 15:38:46 +0100

    updating forms

--- Application/auth/forms.py
+++ Application/auth/forms.py
@@ -1,20 +1,23 @@
 from wtforms import Form, PasswordField, validators, SubmitField, StringField
 
-#using wtforms to create registration and login forms
-#email is a place holder for something
+# using wtforms to create registration and login forms
+
+
 class Registration(Form):
-    name=StringField("Name:")
-    username=StringField("Username:", [validators.DataRequired()])
-    email = StringField('Email Address:', [validators.Length(min=6, max=35), validators.Email()])
+    name = StringField("Name:")
+    username = StringField("Username:", [validators.DataRequired()])
+    email = StringField('Email Address:', [
+                        validators.Length(min=6, max=35), validators.Email()])
     password = PasswordField('New Password:', [
         validators.DataRequired(),
         validators.EqualTo('confirm', message='Passwords must match')
     ])
     confirm = PasswordField('Repeat Password:')
-
-    submit=SubmitField("Submit")
+    role = SelectField('Role:', [validators.DataRequired()], choices=[
+                       ('admin', 'Admin'), ('astronaut', 'Astronaut')], validate_choice=True)
+    submit = SubmitField("Submit")
 
 
 class Login(Form):
-    username=StringField("Username:", [validators.DataRequired()])
+    username = StringField("Username:", [validators.DataRequired()])
     password = PasswordField('Your Password', [validators.DataRequired()])
