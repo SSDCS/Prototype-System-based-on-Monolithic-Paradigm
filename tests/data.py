b35dd100b4e00c6e116548ecd72a11a17b019e18
anrichp <anrichp@gmail.com>
2021-06-30 23:12:59 +0100

    remove datetime

--- tests/data.py
+++ tests/data.py
@@ -1,11 +1,8 @@
 import random
-import datetime
-
 
 def generate_temperature():
     temperature = random.randint(18, 21)
     return {
-        "id": datetime.datetime.now(),
         "temperature:": temperature
     }
 
