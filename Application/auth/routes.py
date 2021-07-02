8d3983859f97e99760f14d2f11d79b193bf5facb
anrichp <anrichp@gmail.com>
2021-07-02 15:49:30 +0100

    remove un-used import statement

--- Application/auth/routes.py
+++ Application/auth/routes.py
@@ -3,7 +3,6 @@ Authentication routes
 """
 from flask import Flask, render_template, url_for, session, request, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
 from Application import db
 from Application import bcrypt
 from Application.auth.forms import Login, Registration
