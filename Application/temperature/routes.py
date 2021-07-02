ac180adb62e2b0c2ac871269b66a2002c4202a62
lucosmo <lukasz.kosmaczewski@gmail.com>
2021-07-02 21:29:28 +0100

    trying to push live data from kafka

--- Application/temperature/routes.py
+++ Application/temperature/routes.py
@@ -3,14 +3,23 @@ Temperature
 """
 from flask import render_template
 from Application.temperature import bp
-from tests import consumers
+from kafka import KafkaConsumer
+import json
+
+def con():
+    consumer = KafkaConsumer('temperature',bootstrap_servers=['localhost:9092'])
+
+    for msg in consumer:
+        val = json.loads(msg.value)
+        print(val["temperature:"])
 
 #@bp.route('/')
 #def show_temp():
 #    return render_template('templates/temperature.html')
 @bp.route('/temperature', methods=['GET', 'POST'])
 def temperature1():
-    return render_template('templates/temperature.html')
+    return render_template('temperature.html')
+
 @bp.route('/temperature/<value>', methods=['GET', 'POST'])
 def temperature2(value):
     return "Temperature is: "+value
