8f0d154514234a96b4303ec36fc8dba00ee76dcf
lucosmo <lukasz.kosmaczewski@gmail.com>
2021-07-03 09:03:34 +0100

    route /temperature shows temp. high temp alert

--- Application/temperature/routes.py
+++ Application/temperature/routes.py
@@ -1,23 +1,33 @@
 """
 Temperature
 """
-from flask import render_template
+from flask import render_template, Response, redirect, request, url_for
 from Application.temperature import bp
+import itertools
+import time
 from kafka import KafkaConsumer
 import json
 
-def con():
-    consumer = KafkaConsumer('temperature',bootstrap_servers=['localhost:9092'])
 
-    for msg in consumer:
-        val = json.loads(msg.value)
-        print(val["temperature:"])
+consumer = KafkaConsumer('temperature',bootstrap_servers=['localhost:9092'])
+
+    
+        
+    
 
 #@bp.route('/')
 #def show_temp():
 #    return render_template('templates/temperature.html')
 @bp.route('/temperature', methods=['GET', 'POST'])
 def temperature1():
+    if request.headers.get('accept') == 'text/event-stream':
+        def events():
+            for msg in consumer:
+                val = json.loads(msg.value)
+                yield "data: %d\n\n" % (val["temperature:"])
+                #time.sleep(.1)  # an artificial delay
+        return Response(events(), content_type='text/event-stream')
+    redirect('/temperature')
     return render_template('temperature.html')
 
 @bp.route('/temperature/<value>', methods=['GET', 'POST'])
