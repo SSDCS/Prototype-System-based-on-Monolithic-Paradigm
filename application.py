a2089212ce80f20fa3e0a1c1c1d6728089db997b
anrichp <anrichp@gmail.com>
2021-07-04 13:23:18 +0100

    add __main__ to allow for running the application from the application.py file

--- application.py
+++ application.py
@@ -2,3 +2,6 @@ from Application import create_app, db
 from Application.models import Admin, Astronaut
 
 app = create_app()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
