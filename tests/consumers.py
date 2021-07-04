64d10e8787ab83b88e383b68077bcba21517ff8d
anrichp <anrichp@gmail.com>
2021-07-04 19:52:10 +0100

    update consumer

--- tests/consumers.py
+++ tests/consumers.py
@@ -1,9 +1,15 @@
 from kafka import KafkaConsumer
 import json
 
+consumer = KafkaConsumer('temperature', bootstrap_servers=['127.0.0.1:9092'])
+
+
 def con():
-    consumer = KafkaConsumer('temperature',bootstrap_servers=['localhost:9092'])
 
     for msg in consumer:
         val = json.loads(msg.value)
         print(val["temperature:"])
+
+
+if __name__ == '__main__':
+    con()
