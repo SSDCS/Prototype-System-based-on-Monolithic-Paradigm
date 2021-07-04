78b80c2ceea844d9b4bae678471a81582509b74c
anrichp <anrichp@gmail.com>
2021-07-04 13:01:52 +0100

    fix line spacing

--- Application/temperature/routes.py
+++ Application/temperature/routes.py
@@ -9,14 +9,11 @@ from kafka import KafkaConsumer
 import json
 
 
-consumer = KafkaConsumer('temperature',bootstrap_servers=['localhost:9092'])
+consumer = KafkaConsumer('temperature', bootstrap_servers=['localhost:9092'])
 
-    
-        
-    
 
-#@bp.route('/')
-#def show_temp():
+# @bp.route('/')
+# def show_temp():
 #    return render_template('templates/temperature.html')
 @bp.route('/temperature', methods=['GET', 'POST'])
 def temperature1():
@@ -25,10 +22,11 @@ def temperature1():
             for msg in consumer:
                 val = json.loads(msg.value)
                 yield "data: %d\n\n" % (val["temperature:"])
-                #time.sleep(.1)  # an artificial delay
+                # time.sleep(.1)  # an artificial delay
         return Response(events(), content_type='text/event-stream')
     redirect('/temperature')
     return render_template('temperature.html')
+
 
 @bp.route('/temperature/<value>', methods=['GET', 'POST'])
 def temperature2(value):
