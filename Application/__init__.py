265af0cd976ec1e81808b02f6cf87cef0791bad1
anrichp <anrichp@gmail.com>
2021-07-04 13:11:22 +0100

    replace bcrypt with argon2

--- Application/__init__.py
+++ Application/__init__.py
@@ -13,7 +13,7 @@ def create_app(config_class=Config):
     app.config.from_object(config_class)
 
     db.init_app(app)
-    bcrypt.init_app(app)
+    ph.init_app(app)
 
     from Application.auth import bp as auth_bp
     app.register_blueprint(auth_bp, url_prefix='/auth')
