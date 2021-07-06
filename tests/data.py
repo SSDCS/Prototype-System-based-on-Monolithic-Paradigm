6aec2d46d7bb3a9815aeb5a61f14cee1c8651489
anrichp <anrichp@gmail.com>
2021-07-06 21:15:46 +0100

    append schema

--- tests/data.py
+++ tests/data.py
@@ -5,8 +5,15 @@ from datetime import datetime
 def generate_temperature():
     temperature = random.randint(18, 21)
     return {
-        "id": datetime.now(),
-        "Temperature": temperature
+        "schema": {
+            "type": "struct", "version": 1, "fields": [
+                {"field": "ID", "type": "int"},
+                {"field": "Temperature", "type": "int"}
+            ]},
+        "payload": {
+            "ID": datetime.now(),
+            "Temperature": temperature,
+        }
     }
 
 
