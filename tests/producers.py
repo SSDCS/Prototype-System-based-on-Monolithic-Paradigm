e774b0786592c17cb6a78e1119e6fc3b0dbc187b
anrichp <anrichp@gmail.com>
2021-07-06 20:51:15 +0100

    fix formatting

--- tests/producers.py
+++ tests/producers.py
@@ -8,12 +8,14 @@ from data import generate_temperature
 import time
 import datetime
 
+
 def defaultconverter(o):
-  if isinstance(o, datetime.datetime):
-      return o.__str__()
+    if isinstance(o, datetime.datetime):
+        return o.__str__()
+
 
 temperature_producer = KafkaProducer(bootstrap_servers=['127.0.0.1:9092'],
-                         value_serializer=lambda m: json.dumps(m, default=defaultconverter).encode('ascii'))
+                                     value_serializer=lambda m: json.dumps(m, default=defaultconverter).encode('ascii'))
 
 if __name__ == "__main__":
     while 1 == 1:
