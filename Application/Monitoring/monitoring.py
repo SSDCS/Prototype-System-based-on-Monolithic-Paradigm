fdf7421f40d282e5a26458c048eb9dc239954eee
anrichp <anrichp@gmail.com>
2021-07-10 12:28:02 +0100

    create some test code to monitoring service

--- Application/Monitoring/monitoring.py
+++ Application/Monitoring/monitoring.py
@@ -1,6 +1,33 @@
 from kafka import KafkaConsumer
 
-temperature_consumer = KafkaConsumer('temperature', bootstrap_servers=['127.0.0.1:9092'])
-electrical_consumer = KafkaConsumer('electrical', bootstrap_servers=['127.0.0.1:9092'])
+temperature_consumer = KafkaConsumer(
+    'temperature', bootstrap_servers=['127.0.0.1:9092'])
+electrical_consumer = KafkaConsumer(
+    'electrical', bootstrap_servers=['127.0.0.1:9092'])
 oxygen_consumer = KafkaConsumer('oxygen', bootstrap_servers=['127.0.0.1:9092'])
 fire_consumer = KafkaConsumer('fire', bootstrap_servers=['127.0.0.1:9092'])
+
+
+class Temperature():
+    ALARM = False
+
+    def monitor_temperature():
+        for temperature in temperature_consumer:
+            if temperature["Payload"]["Temperature"] < 19:
+                print("Temperature too Low")
+            elif temperature["Payload"]["Temperature"] > 20:
+                print ("Temperature too High")
+            else: 
+                print("Temperature perfect")
+
+
+class Electrial():
+    pass
+
+
+class oxygen():
+    pass
+
+
+class fire():
+    pass
