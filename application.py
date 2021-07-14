72002b2136baa1150444775be664eb6932552eb3
anrichp <anrichp@gmail.com>
2021-07-14 19:16:17 +0100

    test whether we can trigger threads from athe flask main method

--- application.py
+++ application.py
@@ -1,13 +1,22 @@
 from Application import create_app, db
 from Application.models import Admin, Astronaut
 from Application.Monitoring.monitoring import Temperature, Electrial, Oxygen, Fire
+from threading import Thread
 
-temperature = Temperature()
-electrical = Electrial()
-oxygen = Oxygen()
-fire = Fire()
 
 app = create_app()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
+    t1 = Thread(target=Temperature)
+    t2 = Thread(target=Electrial)
+    t3 = Thread(target=Oxygen)
+    t4 = Thread(target=Fire)
+    t1.setDaemon(True)
+    t2.setDaemon(True)
+    t3.setDaemon(True)
+    t4.setDaemon(True)
+    t1.start()
+    t2.start()
+    t3.start()
+    t4.start()
