337b8708a453e1d6b6e75c8bfceae8956c8aa0da
anrichp <anrichp@gmail.com>
2021-07-18 20:01:05 +0100

    update configs to include bings

--- config.py
+++ config.py
@@ -7,4 +7,8 @@ load_dotenv(os.path.join(basedir, '.env'))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'thisissupposedtobemysecret' #configure a secret key
     SQLALCHEMY_DATABASE_URI = 'sqlite:///users.db' #configure users database
+    SQLALCHEMY_BINDS = {
+    'healthDB':'sqlite:///healthDB.db',
+    'engineering_db':'sqlite:///engineering.db'
+    }
     SQLALCHEMY_TRACK_MODIFICATIONS = False
