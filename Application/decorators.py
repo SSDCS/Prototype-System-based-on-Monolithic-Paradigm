f18165f035b00d747526c2e269d3693c0589f3ee
lucosmo <lukasz.kosmaczewski@gmail.com>
2021-07-19 11:25:25 +0100

    comments for decorators and models

--- Application/decorators.py
+++ Application/decorators.py
@@ -2,8 +2,10 @@ from functools import wraps
 from flask import url_for, redirect, flash, session
 
 def login_required(f):
+    #decorator wrap make function wrap to look like f funcion  
     @wraps(f)
     def wrap(*args, **kwargs):
+        #checking if username is logged already in
         if "username" in session:
             return f(*args, **kwargs)
         else:
